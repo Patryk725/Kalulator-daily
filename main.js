e1dffb0b0c2f3d69bacce27bc5e2b4bc0c745d40
Patryk725 <68645604+Patryk725@users.noreply.github.com>
2021-01-19 23:58:56 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+    min = 15;
+    max = 250;
+    x = Math.random() * (max - min) + min;
+    x = Math.round(x);
+    x = Math.ceil(x / 10) * 10;
+
+
+document.getElementById("push-ups").innerHTML = "Pompek: " + x; 
+
+ile = 0;
+
+document.getElementById("made").innerHTML = "Zrobiłeś: " + ile;
+
+function add()
+{
+    ile += 10;
+    document.getElementById("made").innerHTML = "Zrobiłeś: " + ile;
+
+    if(ile>x){
+        alert("Już nadrobiłeś pompki")
+        ile = ile - 10;
+        document.getElementById("made").innerHTML = "Zrobiłeś: " + ile;
+    }
+}
+
+function minus()
+{
+    ile = ile - 10;
+    document.getElementById("made").innerHTML = "Zrobiłeś: " + ile;
+    if(ile<0)
+    {
+        alert("Po co schodzisz poniżej zera?")
+        ile += 10;
+        document.getElementById("made").innerHTML = "Zrobiłeś: " + ile;
+    }
+
+}
