e5c3dbb2f39cc25ad649082b969b603a53aa0cc8
Patryk725 <68645604+Patryk725@users.noreply.github.com>
2021-01-20 11:32:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-    min = 50;
-    max = 200;
+    min = 15;
+    max = 250;
     x = Math.random() * (max - min) + min;
     x = Math.round(x);
     x = Math.ceil(x / 10) * 10;
@@ -33,5 +33,10 @@ function minus()
         ile += 10;
         document.getElementById("made").innerHTML = "Zrobiłeś: " + ile;
     }
+}
 
+function zero()
+{
+    ile=0;
+    document.getElementById("made").innerHTML = "Zrobiłeś: " + ile;
 }
